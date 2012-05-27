908f97bee0638d6d5431185eadb26a9a972983a4
Bastiaan Zapf <bzapf@online.de>
2012-05-27 20:37:46 +0200

    Parser for .lrn-Files

---
+++ Som/Lrn.hs
@@ -0,0 +1,34 @@
+
+module Som.Lrn (DataSet,readLrnFile) where
+
+import System.IO
+import Data.List.Split
+import Data.Maybe
+
+type DataSet = (Int,[Float])
+
+nonBlanks = split (dropBlanks $ dropDelims $ oneOf "\t ")
+
+readLrnLine :: Handle -> IO (Maybe DataSet)
+readLrnLine handle = do l <- hGetLine handle                        
+                        let nums=nonBlanks l
+                        if (head l == '%')
+                        then return Nothing
+                        else return (Just ((read $ head $ nums),
+                                           (map read $ tail nums)))
+                        
+readLrnLines :: Handle -> IO [DataSet]
+readLrnLines handle = do eof <- hIsEOF handle
+                         if eof 
+                         then return []
+                         else do d <- (readLrnLine handle)
+                                 ds <- (readLrnLines handle)
+                                 case d of 
+                                   Nothing -> return ds
+                                   Just d_ -> return (d_:ds)
+
+                                                    
+readLrnFile :: String -> IO [DataSet]
+readLrnFile path = do handle <- openFile path ReadMode
+                      readLrnLines handle
+                 
